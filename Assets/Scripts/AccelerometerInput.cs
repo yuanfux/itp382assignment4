d71aa9b5aca204dcd4719a9831cc42dbe28aa38e
Yuan Fu <fuyuanx@gmail.com>
2015-02-12 21:02:03 -0800

    Part 6 finished

---
+++ Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,47 @@
+﻿
+//Error occurs at line "lowPassValue = Mathf.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);"
+//Mathf.Lerp only works for float types. This should be changed to Vector3.Lerp function which takes in Vector3 variables.
+
+
+using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+	public float speed=10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float x = 0;
+		float z = 0;
+		bool hasKeyboard = false;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+						hasKeyboard = true;
+						z += 0.1f;
+				} else if (Input.GetKey (KeyCode.DownArrow)) {
+						hasKeyboard = true;
+						z -= 0.1f;
+
+				} else if (Input.GetKey (KeyCode.LeftArrow)) {
+						hasKeyboard = true;
+						x -= 0.1f;
+				} else if (Input.GetKey (KeyCode.RightArrow)) {
+						hasKeyboard = true;
+						x += 0.1f;
+				}
+
+		if (!hasKeyboard) {
+			x=Input.acceleration.x;
+			z=-Input.acceleration.z;
+				}
+
+		transform.Translate (x, 0, z);
+
+	}
+}
---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+		bool suppMultiTouch = Input.multiTouchEnabled;
+
+		print ("MultiTouchSupport: " + suppMultiTouch);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int numTouch = Input.touchCount;
+
+		if (numTouch > 0) {
+
+			for(int i=0 ;i<numTouch;i++){
+
+				Touch touch=Input.GetTouch(i);
+
+				if(touch.phase ==TouchPhase.Began){
+
+					Ray sRay=Camera.main.ScreenPointToRay(touch.position);
+
+					RaycastHit hit;
+
+					if(Physics.Raycast(sRay, out hit)){
+						if(hit.collider.gameObject.tag=="ball"){
+						Destroy(hit.collider.gameObject);
+						}
+					}
+				}
+
+			}
+
+		}
+
+		else if(Input.GetMouseButtonDown(0)){
+			Ray sRay=Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			RaycastHit hit;
+			
+			if(Physics.Raycast(sRay, out hit)){
+				
+				if(hit.collider.gameObject.tag=="ball"){
+					Destroy(hit.collider.gameObject);
+				}
+				
+			}
+			
+		}
+
+
+	}
+
+	
+}
